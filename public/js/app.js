853aa00fe11285d1a4e26e1da02e17cdabb6ab90
HaileyR <hangyeolryu@gmail.com>
2014-12-29 10:44:49 -0800

    refactored

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
   game = new Game();
-// debugger
   game.board_seq.forEach(function(element, index){
     $('#'+index).append("<span class=\"insideTd\">"+element+"</span>");
   });
@@ -8,16 +7,15 @@ $(document).ready(function() {
   setTimeout(function(){ $("span").fadeOut(); }, 5000);
   setTimeout(function(){ $('#question').text(game.question()+" = ").fadeIn(); }, 5500);
 
-
   var clickCount = 0;
   var clickedTds = [];
+
   $('#hint').on('click', function(){
-    console.log('hint works');
     $(".insideTd").show();
     setTimeout(function(){ $(".insideTd").fadeOut(); }, 2000);
   });
+
   $('#board').on('click', 'td', function(event){
-    // debugger
     $(event.target.firstChild).show();
     clickCount+=1;
     if(clickCount<4){
@@ -25,49 +23,29 @@ $(document).ready(function() {
       clickedTds.push(this.id);
       if(clickCount===1 || clickCount===3){
         if(typeof game.board_seq[this.id] === 'string'){
-          // debugger
-          setTimeout(function(){ $('.insideTd').fadeOut(); }, 100);
-          alert('Wrong! Click Number, Operation, and Number.');
-          $('#score').text('Score: '+game.score+'/'+game.outOf);
-          $('#question').text(game.question()+" = ");
-          clickCount=0;
-          clickedTds = [];
+          resetQuestion('Wrong! Click Number, Operation, and Number.');
         }else if(clickCount===3){
           if(game.result(parseInt($('#question')[0].textContent), clickedTds)){
-            setTimeout(function(){ $('.insideTd').fadeOut(); }, 100);
-            alert('Good!');
-            $('#score').text('Score: '+game.score+'/'+game.outOf);
-            $('#question').text(game.question()+" = ");
-            clickCount=0;
-            clickedTds = [];
+            resetQuestion('Good! :)');
           }else{
-            setTimeout(function(){ $('.insideTd').fadeOut(); }, 100);
-            alert('Wrong!........');
-            $('#score').text('Score: '+game.score+'/'+game.outOf);
-            $('#question').text(game.question()+" = ");
-            clickCount=0;
-            clickedTds = [];
+            resetQuestion('Wrong!........');
           };
         };
       }else if(clickCount===2){
         if(typeof game.board_seq[this.id] === 'number'){
-          setTimeout(function(){ $('.insideTd').fadeOut(); }, 100);
-          alert('Wrong! Click Number, Operation, and Number.');
-          $('#score').text('Score: '+game.score+'/'+game.outOf);
-          $('#question').text(game.question()+" = ");
-          clickCount=0;
-          clickedTds = [];
+          resetQuestion('Wrong! Click Number, Operation, and Number.');
         };
       };
-
-
     };
-
   });
-
 });
 
 
-
-
-
+function resetQuestion(msg){
+  alert(msg);
+  setTimeout(function(){ $('.insideTd').fadeOut(); }, 100);
+  $('#score').text('Score: '+game.score+'/'+game.outOf);
+  $('#question').text(game.question()+" = ");
+  clickCount=0;
+  clickedTds = [];
+};
